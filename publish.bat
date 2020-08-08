9ba745c43ea3c5ad37fe5e4845c64c867c30ce39
venyowong <venyowong@163.com>
2020-08-08 09:02:29 +0800

    修改发布脚本

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
-cd src
 del /f /s /q publish-linux\*.*
 rd /s /q publish-linux
-dotnet publish -r linux-x64 /p:PublishSingleFile=true -c Release -o publish-linux
 del /f /s /q publish-win\*.*
 rd /s /q publish-win
-dotnet publish -r win-x64 /p:PublishSingleFile=true -c Release -o publish-win
+cd src
+dotnet publish -r linux-x64 /p:PublishSingleFile=true -c Release -o ../publish-linux
+dotnet publish -r win-x64 /p:PublishSingleFile=true -c Release -o ../publish-win
 pause
